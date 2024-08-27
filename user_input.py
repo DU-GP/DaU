dedb8057f30751d7a3d7ae77b26cf5e9a255edff
DU-GP <udavidnwokoma@gmail.com>
2024-08-27 08:06:11 -0700

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input("What is your name?: ")
+age = input("How old are you?: ")
+location = input("Where do you live?: ")
+print("Hello!, " + name + " you are " + age + " old, " + " you live at " + location)
